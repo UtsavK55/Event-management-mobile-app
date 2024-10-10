f85c958303f10fc55ec35871c4e8eb7d9dea1e75
UtsavK55 <utsav@gkmit.co>
2024-10-10 13:30:11 +0530

    resolve comments

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 
 import React from 'react';
+import { Text } from 'react-native-reanimated/lib/typescript/Animated';
 
 function App(): React.JSX.Element {
 
   return (
-   <></>
+   <Text>App</Text>
   );
 }
 
